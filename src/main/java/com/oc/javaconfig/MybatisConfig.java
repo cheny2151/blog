469f2d82434a3971b0a5d7602dc4517b4ded6447
cheny2151 <cheny21512151@gmail.com>
2018-03-14 22:58:07 +0800

    fix 注入datasource

--- src/main/java/com/oc/javaconfig/MybatisConfig.java
+++ src/main/java/com/oc/javaconfig/MybatisConfig.java
@@ -1,26 +1,28 @@
 package com.oc.javaconfig;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.io.IOException;
 
 /**
  * mybatis配置
  */
 
-//@Configuration
-//@MapperScan(basePackages = "com.my.boot.test.mapper", sqlSessionTemplateRef  = "mybatisMasterSqlSessionTemplate")
+@Configuration
+@Import(RootConfig.class)
+@MapperScan(basePackages = "com.my.boot.test.mapper", sqlSessionTemplateRef = "mybatisMasterSqlSessionTemplate")
 public class MybatisConfig {
-    @Resource
-    ComboPooledDataSource dataSource;
 
+    @Autowired
     @Bean(name = "sqlSessionFactory")
-    public SqlSessionFactoryBean sqlSessionFactory() throws IOException {
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws IOException {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/*.xml"));
