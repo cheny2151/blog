a4d7234f832fcdab875b366389a32175efe5f073
Ocean <243566714@qq.com>
2018-03-14 23:11:55 +0800

    [fix] 半成品mybatis

--- src/main/java/com/oc/javaconfig/MybatisConfig.java
+++ src/main/java/com/oc/javaconfig/MybatisConfig.java
@@ -1,7 +1,9 @@
 package com.oc.javaconfig;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +19,7 @@ import java.io.IOException;
 
 @Configuration
 @Import(RootConfig.class)
-@MapperScan(basePackages = "com.my.boot.test.mapper", sqlSessionTemplateRef = "mybatisMasterSqlSessionTemplate")
+@MapperScan(basePackages = "com.oc.dao.mDao", sqlSessionTemplateRef = "sqlSessionTemplate")
 public class MybatisConfig {
 
     @Autowired
@@ -29,9 +31,19 @@ public class MybatisConfig {
         return sqlSessionFactoryBean;
     }
 
-    /*public SqlSessionTemplate sqlSessionTemplate() throws IOException {
-        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory());
+    @Autowired
+    @Bean(name = "sqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(DataSource dataSource) throws Exception {
+        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory(dataSource).getObject());
         return sqlSessionTemplate;
-    }*/
+    }
+
+    @Bean(name = "mapperScannerConfigurer")
+    public MapperScannerConfigurer mapperScannerConfigurer(){
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        mapperScannerConfigurer.setBasePackage("com.oc.dao.mDao");
+        return mapperScannerConfigurer;
+    }
 
 }
