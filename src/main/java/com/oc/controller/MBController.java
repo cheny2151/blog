b68b97b5af99be15b31cdbdaac456afb2432c115
Ocean <243566714@qq.com>
2018-03-15 16:58:12 +0800

    [fix] 柳暗花明又一村 mybatis

---
+++ src/main/java/com/oc/controller/MBController.java
@@ -0,0 +1,22 @@
+package com.oc.controller;
+
+import com.oc.entity.User;
+import com.oc.service.mService.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("mBController")
+@RequestMapping(value = "/mybatis")
+public class MBController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/test")
+    public void test(){
+        System.out.println("=========come in==========");
+        User user = userService.getUser("user");
+        System.out.println(user.toString());
+    }
+}
--- src/main/java/com/oc/service/mService/UserService.java
+++ src/main/java/com/oc/service/mService/UserService.java
@@ -2,16 +2,17 @@ package com.oc.service.mService;
 
 import com.oc.entity.User;
 import com.oc.dao.mDao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-//@Service
+@Service
 public class UserService {
-    @Resource
+    @Autowired
     private UserDao userDao;
 
-    public User getUserDao(String account) {
+    public User getUser(String account) {
         return userDao.getUser(account);
     }
 }
