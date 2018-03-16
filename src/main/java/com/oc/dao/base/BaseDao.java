463b31d53e5023c0d49bce0fdfd9ec9478ebc1c6
2151 <478241123@qq.com>
2018-03-16 11:35:46 +0800

    update basedao

---
+++ src/main/java/com/oc/dao/base/BaseDao.java
@@ -0,0 +1,50 @@
+package com.oc.dao.base;
+
+import com.oc.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseDao<T extends BaseEntity, ID extends Serializable> {
+
+    /**
+     * 根据id查找对象
+     */
+    T find(Long id);
+
+    /**
+     * 查找所有实体
+     */
+    List<T> findAll();
+
+    /**
+     * 保存
+     */
+    void persist(T entity);
+
+    /**
+     * 更新
+     */
+    void merge(T entity);
+
+    /**
+     * 删除
+     */
+    void remove(ID id);
+
+    /**
+     * 批量删除
+     */
+    void remove(ID... id);
+
+    /**
+     * flush
+     */
+    void flush();
+
+    /**
+     * 根据实体获取id
+     */
+    ID getIdentifire(T entity);
+
+}
